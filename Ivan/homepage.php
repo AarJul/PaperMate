f0306831ecd54e8da382c3f530e4bbf19fbfab8e
ivank101 <ivankurniawan2012@gmail.com>
2023-12-07 09:40:59 +0900

    s

--- Ivan/homepage.php
+++ Ivan/homepage.php
@@ -10,11 +10,16 @@
 <head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage/title>
+    <title>Homepage</title>
 </head>
 <body>
 
-
+    <h1>List of procedures</h1>
+    <?php while($row = $documents->fetch_assoc()): ?>
+        <div>
+            <img src="<?php echo $row['documentpics']; ?>">
+        </div>
+    <?php endwhile; ?>
     
 </body>
 </html>
