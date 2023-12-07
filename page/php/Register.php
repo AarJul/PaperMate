b85e9e5f2a9ace283888de74f21c17c510affb63
ivank101 <ivankurniawan2012@gmail.com>
2023-12-07 10:37:12 +0900

    s

---
+++ page/php/Register.php
@@ -0,0 +1,37 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "papermate";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$telephone = $_POST['tel'];
+$email = $_POST['email'];
+$password = $_POST['pwd'];
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+$language = $_POST['language'];
+
+$stmt = $conn->prepare("INSERT INTO user (username, telephone, email, password, language) 
+                        VALUES (?, ?, ?, ?, ?)");
+
+if (!$stmt) {
+    die('Error in statement preparation: ' . $conn->error);
+}
+
+$stmt->bind_param("sssss", $name, $telephone, $email, $hashedPassword, $language);
+
+if ($stmt->execute()) {
+    echo "Registration successful!";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
