99c25dfa22cd5fcce8c7377c59aa1a14e444bc6e
mintie98 <thang3dinhat@gmail.com>
2023-12-14 10:04:41 +0900

    lang change

--- Min/lang.js
+++ Min/lang.js
@@ -1,71 +1,70 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const languageOptions = document.querySelectorAll('.change-lang');
-    let languageJSON = {};
-
-    // Hàm tải dữ liệu JSON dựa trên ngôn ngữ đã chọn
-    function loadLanguageJSON(lang, callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.overrideMimeType("application/json");
-        xhr.open('GET', 'json/' + lang + '.json', true); // Sử dụng ngôn ngữ làm tên file JSON
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                callback(xhr.responseText);
-            }
-        };
-        xhr.send(null);
-    }
-
-    // Chuyển đổi ngôn ngữ khi được chọn
-    function setLanguage(lang) {
-        localStorage.setItem('selectedLang', lang);
-        loadLanguageJSON(lang, function(response) {
-            languageJSON = JSON.parse(response);
-            displayLanguageContent(lang);
-            const dropdownButton = document.querySelector('.dropdown-toggle');
-            dropdownButton.textContent = getLanguageText(lang);
-        });
-    }
-
-    // Hiển thị nội dung ngôn ngữ
-    function displayLanguageContent(lang) {
-        const selectedData = languageJSON[lang];
-        document.querySelector('h1').textContent = selectedData.title;
-        document.querySelector('h4').textContent = selectedData.subtitle;
-        document.querySelector('input[name="email"]').setAttribute('placeholder', selectedData.emailPlaceholder);
-        document.querySelector('input[name="password"]').setAttribute('placeholder', selectedData.passwordPlaceholder);
-        document.querySelector('label[for="pass_save"]').textContent = selectedData.rememberPasswordLabel;
-        document.querySelector('button').textContent = selectedData.loginButton;
-    }
-
-    // Xử lý sự kiện chọn ngôn ngữ
-    languageOptions.forEach(function(option) {
-        option.addEventListener('click', function(event) {
-            event.preventDefault();
-            const lang = this.getAttribute('data-lang');
-            console.log('Language Clicked:', lang);
-            setLanguage(lang);
-        });
-    });
-
-    // Lấy văn bản ngôn ngữ
-    function getLanguageText(lang) {
-        switch (lang) {
-            case 'en':
-                return 'English';
-            case 'vi':
-                return 'Tiếng Việt';
-            case 'jp':
-                return '日本語';
-            case 'indo':
-                return 'Indonesia';
-            default:
-                return 'Select Language';
-        }
-    }
-
-    // Kiểm tra ngôn ngữ đã được chọn trước đó
-    const selectedLang = localStorage.getItem('selectedLang');
-    if (selectedLang) {
-        setLanguage(selectedLang);
-    }
-});
+const languageData = {
+    en: {
+      title: "Login",
+      subtitle: "If you don't have an account, register here",
+      emailPlaceholder: "Enter your email",
+      passwordPlaceholder: "Enter your password",
+      rememberPasswordLabel: "Remember password",
+      loginButton: "Login",
+    },
+    vi: {
+      title: "Đăng nhập",
+      subtitle: "Nếu bạn chưa có tài khoản, đăng ký ở đây",
+      emailPlaceholder: "Nhập email của bạn",
+      passwordPlaceholder: "Nhập mật khẩu của bạn",
+      rememberPasswordLabel: "Ghi nhớ mật khẩu",
+      loginButton: "Đăng nhập",
+    },
+    jp: {
+        title: "ログイン",
+        subtitle: "アカウントをお持ちでない方は、新規登録",
+        emailPlaceholder: "メールアドレスを入力",
+        passwordPlaceholder: "パスワードを入力",
+        rememberPasswordLabel: "パスワードを記憶する",
+        loginButton: "ログイン",
+      },
+    indo: {
+        title: "Masuk",
+        subtitle: "Jika Anda belum memiliki akun, daftar di sini",
+        emailPlaceholder: "Masukkan email Anda",
+        passwordPlaceholder: "Masukkan kata sandi Anda",
+        rememberPasswordLabel: "Ingat kata sandi",
+        loginButton: "Masuk",
+      },
+  };
+  
+  const defaultLanguage = "en"; // Set your default language here
+  
+  let currentLanguage = defaultLanguage;
+  
+  // Change language when clicking on dropdown menu items
+  const changeLanguage = (event) => {
+    currentLanguage = event.target.dataset.lang;
+    updateLanguageContent();
+  };
+  
+  // Update the UI content with the current language
+  const updateLanguageContent = () => {
+    const languageDataObj = languageData[currentLanguage];
+    const titleEl = document.querySelector("h1");
+    const subtitleEl = document.querySelector("h4");
+    const emailInput = document.querySelector("input[name='email']");
+    const passwordInput = document.querySelector("input[name='password']");
+    const rememberPasswordLabel = document.querySelector("label[for='pass_save']");
+    const loginBtn = document.querySelector("button[name='login']");
+  
+    titleEl.textContent = languageDataObj.title;
+    subtitleEl.textContent = languageDataObj.subtitle;
+    emailInput.placeholder = languageDataObj.emailPlaceholder;
+    passwordInput.placeholder = languageDataObj.passwordPlaceholder;
+    rememberPasswordLabel.textContent = languageDataObj.rememberPasswordLabel;
+    loginBtn.textContent = languageDataObj.loginButton;
+  };
+  
+  // Attach event listener to dropdown menu items
+  const dropdownItems = document.querySelectorAll(".change-lang");
+  dropdownItems.forEach((item) => item.addEventListener("click", changeLanguage));
+  
+  // Initialize the UI with the default language
+  updateLanguageContent();
+  
