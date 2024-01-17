b7379246361466362c9429f347a751c896447e2d
AarJul <barongdong3@gmail.com>
2024-01-18 08:51:18 +0900

    modified:   Quan/getAllDoc_json.php 	renamed:    page/VueTest.php -> page/VueTest.html 	modified:   page/allDoc.html 	modified:   page/allDoc.js 	modified:   page/stepNewPost.html

--- page/allDoc.js
+++ page/allDoc.js
@@ -1,30 +1,45 @@
-// Use the CDN version of Axios
-const axios = window.axios;
+const DocumentItem = {
+    setup() {
+        const documents = Vue.ref([]);
 
-const app = Vue.createApp({
-    data() {
+        Vue.onMounted(async () => {
+            try {
+                const response = await axios.get('http://localhost:80/Quan/getAllDoc_json.php');
+                documents.value = response.data.documents;
+                console.log(documents.value);
+            } catch (error) {
+                console.error(error);
+            }
+        });
+
+        const handlePageChange = (id) => {
+            localStorage.setItem("documentid", id)
+            window.location.href = "/page/steps.html"
+        };
+
         return {
-            data: null,
+            documents,
+            handlePageChange, // Expose the method to the template
         };
     },
-    mounted() {
-        axios.get('php/getAllDoc_json.php') // Adjust the path here
-            .then(response => (this.data = response.data))
-            .catch(error => console.error(error));
-    },
-});
-
-app.component('document-item', {
-    props: ['documentname', 'documentpics'],
     template: `
-        <div class="grid-item">
-            <h3>{{ documentname }}</h3>
-            <div class="d-flex">
-                <img :src="documentpics" :alt="title" class="img-fluid mb-3">
-                <p class="mt-auto mb-auto ms-3">{{ description }}</p>
+        <div class="grid-container">
+            <div v-for="document in documents" :key="document.documentid" class="grid-item">
+                <h3>{{ document.documentname }}</h3>
+                <img v-if="document.documentpics !== null" :src="document.documentpics" :alt="document.documentname" class="img-fluid mb-3">
+                <p class="mt-auto mb-auto ms-3">{{ document.documentname }}</p>
+                <button @click="handlePageChange(document.documentid)">isi button sek</button>
             </div>
         </div>
     `,
+};
+
+const app = Vue.createApp({
+    setup() {
+        return {};
+    },
 });
 
+// Register the component globally or within another component
+app.component('document-item', DocumentItem);
 app.mount('#app');
