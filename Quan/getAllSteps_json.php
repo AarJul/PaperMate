97dc7f0c4854541c3bb103e3b2a32fc76c01b55d
Aaron <barongdong3@gmail.com>
2024-01-18 11:09:37 +0900

    modified:   Quan/getAllDoc_json.php 	modified:   Quan/getAllSteps_json.php 	modified:   page/allDoc.html 	modified:   page/php/getAllSteps_json.php 	new file:   page/stepDetail.js 	modified:   page/steps.html 	modified:   page/steps.js

--- Quan/getAllSteps_json.php
+++ Quan/getAllSteps_json.php
@@ -1,12 +1,32 @@
 <?php
-    include 'db_connect.php';
-    session_start();
-    $_SESSION['step_id'] = null;
-    $db = connect_db();
-    $document_id = $_SESSION['document_id'] === null ? $_GET['id'] : $_SESSION['document_id'];
+include 'db_connect.php';
+session_start();
+$_SESSION['step_id'] = null;
+$db = connect_db();
+
+// Fetch documentid from POST request
+$document_id = isset($_POST['documentid']) ? $_POST['documentid'] : null;
+
+$document_id = 1;
+
+if ($document_id === null) {
+    // Handle the case when documentid is not provided
+    echo json_encode(['error' => 'Document ID is missing']);
+} else {
     $_SESSION['document_id'] = $document_id;
 
-    // Chuyển kết quả thành JSON và in ra màn hình
+    // Convert the result to JSON and output it
     header('Content-Type: application/json');
     echo get_document_steps_json($document_id, $db);
+}
+
+// Allow requests only from http://127.0.0.1:5500
+header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
+
+// Allow the following HTTP methods
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+
+// Allow the following headers in the request
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization');
+
 ?>
